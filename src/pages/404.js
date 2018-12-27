59fae6797f9a1ddc3361ddf9327f0d2063cc6a57
Federico Knüssel <fknussel@gmail.com>
2018-12-27 13:20:46 +1100

    Fix issue with 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-export default function NotFoundPage() {
+export default function NotFoundPage(props) {
   return (
     <Layout location={props.location}>
       <h1>Not Found</h1>
