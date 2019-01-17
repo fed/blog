647af7bac3e4cd774acec1cd312960656697dc0c
Federico Knüssel <fknussel@gmail.com>
2019-01-17 17:35:24 +1100

    Render icons next to the post details

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,11 +4,34 @@ import get from 'lodash/get';
 import Layout from '../Layout/Layout';
 import SEO from '../SEO';
 import styles from './Post.module.css';
+import calendarIcon from './calendar.svg';
+import timeIcon from './time.svg';
 
 export default function Post(props) {
     const post = props.data.markdownRemark;
     const { previous, next, slug } = props.pageContext;
-    const postDetails = `${post.frontmatter.date} — ${post.timeToRead} min. read`;
+    const postDetails = (
+        <ul className={styles.detailsList}>
+            <li className={styles.detailsItem}>
+                <img
+                    src={calendarIcon}
+                    alt="Date published"
+                    title="Date published"
+                    className={styles.detailsIcon}
+                />
+                {post.frontmatter.date}
+            </li>
+            <li className={styles.detailsItem}>
+                <img
+                    src={timeIcon}
+                    alt="Time to read"
+                    title="Time to read"
+                    className={styles.detailsIcon}
+                />
+                {post.timeToRead} min.
+            </li>
+        </ul>
+    );
 
     return (
         <Layout className={styles.layout} details={postDetails}>
