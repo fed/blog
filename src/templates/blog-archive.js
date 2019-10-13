8a9c989d79291cfcf0b1f40b560f9b0aa58c3159
Federico Knüssel <fknussel@gmail.com>
2019-10-13 21:58:16 +0200

    Fix issue when sorting collection of posts

--- src/templates/blog-archive.js
+++ src/templates/blog-archive.js
@@ -1,18 +1,12 @@
 import React, { Fragment } from 'react';
 import { graphql } from 'gatsby';
 import get from 'lodash/get';
+import sortBy from 'lodash/sortBy';
 import SEO from '../components/SEO';
 import Grid from '../components/Grid/Grid';
 import Archive from '../components/Archive/Archive';
 import categories from '../data/categories';
 import externalPosts from '../data/external-posts';
-
-function sortPostsByDateDesc(postA, postB) {
-   const dateA = new Date(postA.date);
-   const dateB = new Date(postB.date);
-
-   return dateA > dateB ? -1 : 1;
-}
 
 export default function BlogIndexTemplate(props) {
     const posts = get(props, 'data.allMarkdownRemark.edges').map(post => ({
@@ -21,13 +15,14 @@ export default function BlogIndexTemplate(props) {
         spoiler: get(post, 'node.frontmatter.spoiler'),
         date: get(post, 'node.frontmatter.date'),
         timeToRead: get(post, 'node.timeToRead')
-    })).concat(externalPosts).sort(sortPostsByDateDesc);
+    })).concat(externalPosts);
+    const sortedPosts = sortBy(posts, post => new Date(post.date)).reverse();
 
     return (
         <Fragment>
             <SEO />
             <Grid>
-                <Archive posts={posts} />
+                <Archive posts={sortedPosts} />
             </Grid>
         </Fragment>
     );
