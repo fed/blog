2c6ba0b0dca7e06981f935b41faf23214d42a050
Federico Knüssel <fknussel@gmail.com>
2019-10-13 17:56:21 +0200

    Reuse styles and clean up work

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ export default function Hero(props) {
     return (
         <header className={styles.container}>
             <h1 className={styles.title}>{props.title}</h1>
-            <p className={styles.content}>I'm a software developer with a keen interest in web development and functional programming, and I help software teams ship clean and maintainable code. Feel free to hit me up on <Obfuscate email="hey@fknussel.com" className={styles.link} />.</p>
+            <p className={styles.content}>I'm a Software Engineer with a keen interest in web development, and I help teams ship clean and maintainable code. Feel free to hit me up on <Obfuscate email="hey@fknussel.com" className={styles.link} />.</p>
         </header>
     );
 }
