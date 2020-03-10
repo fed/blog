b8c65c8849220a05071d87a000a3a3fdb90011df
Federico Knüssel <fknussel@gmail.com>
2020-03-10 17:26:38 +1100

    Add custom focus state across all links

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,11 +7,11 @@ import avatarUrl from '../../data/avatar';
 export default function Hero(props) {
     return (
         <header className={styles.container}>
-            <Link to="/">
+            <Link to="/" className={styles.avatarLink}>
                 <img src={avatarUrl} className={styles.avatar} aria-hidden="true" />
             </Link>
             <h1 className={styles.title}>Federico Knüssel</h1>
-            <p className={styles.content}>I'm a Software Engineer with a keen interest in web development, and I help teams ship clean and maintainable code. Feel free to hit me up on <Obfuscate email="hey@fknussel.com" className={styles.link} />.</p>
+            <p className={styles.content}>I'm a Software Engineer with a keen interest in web development, and I help teams ship clean and maintainable code. Feel free to hit me up on <Obfuscate email="hey@fknussel.com" className={styles.emailLink} />.</p>
         </header>
     );
 }
