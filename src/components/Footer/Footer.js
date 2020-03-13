8c7bd30690fcb2870b8e463864969ac1df58fdde
Federico Knüssel <fknussel@gmail.com>
2020-03-13 15:13:15 +1100

    Improve accessibility of Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,24 +4,27 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
     return (
-        <footer className={styles.container}>
-            {links.map(item => (
-                <a
-                    className={styles.link}
-                    href={item.url}
-                    key={item.id}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label={item.name}
-                >
-                    <img
-                        className={styles.icon}
-                        src={item.icon}
-                        alt={item.name}
-                        title={item.name}
-                    />
-                </a>
-            ))}
+        <footer role="contentinfo" className={styles.container}>
+            <ul aria-label="Social media links" className={styles.list}>
+                {links.map(item => (
+                    <li className={styles.listItem}>
+                        <a
+                            className={styles.link}
+                            href={item.url}
+                            key={item.id}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                className={styles.icon}
+                                src={item.icon}
+                                alt={item.name}
+                                title={item.name}
+                            />
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </footer>
     );
 }
