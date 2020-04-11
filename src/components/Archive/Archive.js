629fbaeb438e96921daf70f0e63494a14acad34f
Federico Knüssel <fknussel@gmail.com>
2020-04-11 18:16:35 +1000

    Add Link component

--- src/components/Archive/Archive.js
+++ src/components/Archive/Archive.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { renderLink } from '../Categories/Categories';
+import Link from '../Link/Link';
 import styles from './Archive.module.css';
 
 export default function Archive(props) {
@@ -9,7 +8,11 @@ export default function Archive(props) {
             {props.posts.map((post, index) => (
                 <article key={index} className={styles.post}>
                     <p className={styles.date}>{post.date}</p>
-                    <h2 className={styles.title}>{renderLink(post)}</h2>
+                    <h2 className={styles.title}>
+                        <Link to={post.url || post.slug} isExternal={post.isExternal}>
+                            {post.title}
+                        </Link>
+                    </h2>
                     <p className={styles.spoiler} dangerouslySetInnerHTML={{ __html: post.spoiler }} />
                 </article>
             ))}
