da2a4bcf0adfd351e1756dea4c0cb85dce6dfbe1
Federico Knüssel <fknussel@gmail.com>
2021-12-02 10:57:27 +1100

    Enable gatsby-remark-embedder plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,9 @@ module.exports = {
                             inlineCodeMarker: '÷',
                         },
                     },
+                    {
+                        resolve: `gatsby-remark-embedder`,
+                    },
                 ],
             },
         },
