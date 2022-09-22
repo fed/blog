5255bc5d07118084f86b26a15a591f8adbb47f62
Federico Knüssel <fknussel@gmail.com>
2022-09-22 15:50:22 +1000

    Ugprade to React 18

--- loadershim.js
+++ loadershim.js
@@ -1,3 +1,9 @@
+import { TextDecoder, TextEncoder } from 'util';
+
 global.___loader = {
     enqueue: jest.fn(),
 };
+
+// This is needed so that @wojtekmaj/enzyme-adapter-react-17 works with React 18
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
