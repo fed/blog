a60f547d5a667dd1f501a6c54379bb4ec7650815
Federico Knüssel <fknussel@gmail.com>
2022-12-19 17:57:20 +1100

    Move metadata below the article title

--- src/ui/article.tsx
+++ src/ui/article.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Twemoji } from 'react-emoji-render';
 import styled from 'styled-components';
 
+import { gridSize } from '../styles/constants';
 import { baseTitleStyles } from '../styles/mixins';
 import { Markdown } from './markdown';
 import { Metadata } from './metadata';
@@ -9,6 +10,7 @@ import { CategoryId } from './types';
 
 const Title = styled.h1`
     ${baseTitleStyles};
+    margin: 0 0 ${1.25 * gridSize}px 0;
 `;
 
 interface Props {
@@ -21,10 +23,10 @@ interface Props {
 export const Article: React.FC<Props> = ({ title, date, categoryId, children }) => (
     <article>
         <header>
-            <Metadata date={date} categoryId={categoryId} />
             <Title>
                 <Twemoji svg text={title} />
             </Title>
+            <Metadata date={date} categoryId={categoryId} />
         </header>
         <Markdown>{children}</Markdown>
     </article>
