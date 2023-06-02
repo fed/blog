ae750f93a0216ed25d22eb06ac5ed35b679de66e
Federico Knüssel <fknussel@gmail.com>
2023-06-02 20:54:49 +1000

    Uninstall styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,6 @@ module.exports = {
         'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
         'gatsby-plugin-react-helmet',
-        'gatsby-plugin-styled-components',
         'gatsby-plugin-vanilla-extract',
         'gatsby-plugin-graphql-codegen',
     ],
