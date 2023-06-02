3e534b060c92025746ff25b3bd80a22f280df395
Federico Knüssel <fknussel@gmail.com>
2023-06-02 19:11:11 +1000

    Migrate Navigation component to vanilla-extract

---
+++ src/ui/navigation.css.ts
@@ -0,0 +1,110 @@
+import { style } from '@vanilla-extract/css';
+
+import { baseFocusStateStyle } from '../styles/base.css';
+import {
+    gridSize,
+    colors,
+    fontFamilies,
+    fontSizes,
+    containerDimensions,
+    fontWeights,
+    lineHeights,
+    borderRadius,
+} from '../styles/constants';
+
+export const headerStyle = style({
+    backgroundColor: '#f6f8fa',
+    position: 'relative',
+    selectors: {
+        '&::after': {
+            background:
+                'linear-gradient(180deg, rgba(9, 30, 66, 0.13) 0, rgba(9, 30, 66, 0.13) 1px, rgba(9, 30, 66, 0.08) 1px, rgba(9, 30, 66, 0) 4px)',
+            content: '',
+            height: `${0.5 * gridSize}px`,
+            left: 0,
+            position: 'absolute',
+            right: 0,
+            top: '100%',
+        },
+    },
+});
+
+export const navigationMenuStyle = style({
+    alignItems: 'center',
+    display: 'flex', // Enable scrolling to provide access to all navigation menu items on mobile
+    overflowX: 'scroll',
+    padding: `${1.75 * gridSize}px 0 ${1.75 * gridSize}px ${3 * gridSize}px`,
+    '@media': {
+        [`screen and (min-width: ${containerDimensions.sm})`]: {
+            margin: '0 auto',
+            maxWidth: containerDimensions.lg,
+            overflowX: 'hidden', // Hide the scrollbar on Windows (desktop)
+        },
+    },
+});
+
+export const logoLinkStyle = style([
+    baseFocusStateStyle,
+    {
+        alignItems: 'center',
+        display: 'flex',
+        marginRight: `${3.5 * gridSize}px`,
+        textDecoration: 'none',
+        width: 'max-content',
+    },
+]);
+
+export const avatarStyle = style({
+    height: `${2.5 * gridSize}px`,
+    marginRight: `${1.25 * gridSize}px`,
+    width: `${2.5 * gridSize}px`,
+});
+
+export const siteNameStyle = style({
+    alignItems: 'center',
+    color: colors.navy,
+    fontFamily: fontFamilies.sansSerif,
+    fontSize: fontSizes.sm,
+    fontWeight: fontWeights.bold,
+    lineHeight: lineHeights.sm,
+    whiteSpace: 'nowrap',
+});
+
+export const listStyle = style({
+    display: 'flex',
+    margin: 0,
+    paddingLeft: 0,
+});
+
+export const listItemStyle = style({
+    color: colors.navy,
+    display: 'inline-block',
+    fontSize: fontSizes.xs,
+    fontWeight: fontWeights.normal,
+    lineHeight: lineHeights.sm,
+    listStyle: 'none',
+    marginBottom: '-1px',
+    selectors: {
+        '&:not(:last-child)': {
+            marginRight: `${2 * gridSize}px`,
+        },
+    },
+});
+
+export const linkStyle = style([
+    baseFocusStateStyle,
+    {
+        borderRadius: borderRadius.default,
+        color: colors.navy,
+        display: 'inline-block',
+        fontFamily: fontFamilies.sansSerif,
+        textDecoration: 'none',
+        transition: 'background-color 0.1s ease-out, box-shadow 0.15s cubic-bezier(0.47, 0.03, 0.49, 1.38)',
+        selectors: {
+            '&:hover': {
+                color: colors.blue,
+                textDecoration: 'none',
+            },
+        },
+    },
+]);
--- src/ui/navigation.tsx
+++ src/ui/navigation.tsx
@@ -1,162 +1,74 @@
-import { Link as UnstyledGatsbyLink } from 'gatsby';
+import { Link as GatsbyLink } from 'gatsby';
 import React from 'react';
-import styled, { css } from 'styled-components';
 
 import avatarSrc from './navigation-icon.svg';
-import { colors, containerDimensions, fontFamilies, fontSizes, fontWeights, gridSize, lineHeights } from '../styles/constants';
-import { baseFocusStateStyles } from '../styles/mixins';
-
-const Header = styled.header`
-    background-color: #f6f8fa;
-    position: relative;
-
-    &::after {
-        background: linear-gradient(
-            180deg,
-            rgba(9, 30, 66, 0.13) 0,
-            rgba(9, 30, 66, 0.13) 1px,
-            rgba(9, 30, 66, 0.08) 1px,
-            rgba(9, 30, 66, 0) 4px
-        );
-        content: '';
-        height: ${0.5 * gridSize}px;
-        left: 0;
-        position: absolute;
-        right: 0;
-        top: 100%;
-    }
-`;
-
-const NavigationMenu = styled.nav`
-    align-items: center;
-    display: flex; // Enable scrolling to provide access to all navigation menu items on mobile
-    overflow-x: scroll;
-    padding: ${1.75 * gridSize}px 0 ${1.75 * gridSize}px ${3 * gridSize}px;
-
-    @media (min-width: ${containerDimensions.sm}) {
-        margin: 0 auto;
-        max-width: ${containerDimensions.lg};
-        overflow-x: hidden; // Hide the scrollbar on Windows (desktop)
-    }
-`;
-
-const LogoLink = styled(UnstyledGatsbyLink)`
-    ${baseFocusStateStyles};
-    align-items: center;
-    display: flex;
-    margin-right: ${3.5 * gridSize}px;
-    text-decoration: none;
-    width: max-content;
-`;
-
-const Avatar = styled.img`
-    height: ${2.5 * gridSize}px;
-    margin-right: ${1.25 * gridSize}px;
-    width: ${2.5 * gridSize}px;
-`;
-
-const SiteName = styled.span`
-    align-items: center;
-    color: ${colors.navy};
-    font-family: ${fontFamilies.sansSerif};
-    font-size: ${fontSizes.sm};
-    font-weight: ${fontWeights.bold};
-    line-height: ${lineHeights.sm};
-    white-space: nowrap;
-`;
-
-const List = styled.ul`
-    display: flex;
-    margin: 0;
-    padding-left: 0;
-`;
-
-const ListItem = styled.li`
-    color: ${colors.navy};
-    display: inline-block;
-    font-size: ${fontSizes.xs};
-    font-weight: ${fontWeights.normal};
-    line-height: ${lineHeights.sm};
-    list-style: none;
-    margin-bottom: -1px;
-
-    &:not(:last-child) {
-        margin-right: ${2 * gridSize}px;
-    }
-`;
-
-const baseNavigationLinkStyles = css`
-    ${baseFocusStateStyles};
-    border-radius: 3px;
-    color: ${colors.navy};
-    display: inline-block;
-    font-family: ${fontFamilies.sansSerif};
-    text-decoration: none;
-    transition: background-color 0.1s ease-out, box-shadow 0.15s cubic-bezier(0.47, 0.03, 0.49, 1.38);
-
-    &:hover {
-        color: ${colors.blue};
-        text-decoration: none;
-    }
-`;
-
-const ListItemExternalLink = styled.a`
-    ${baseNavigationLinkStyles};
-`;
-
-const ListItemInternalLink = styled(UnstyledGatsbyLink)`
-    ${baseNavigationLinkStyles};
-`;
+import {
+    headerStyle,
+    navigationMenuStyle,
+    logoLinkStyle,
+    avatarStyle,
+    siteNameStyle,
+    listStyle,
+    listItemStyle,
+    linkStyle,
+} from './navigation.css';
 
 export const Navigation: React.FC = () => (
-    <Header>
-        <NavigationMenu>
-            <LogoLink to="/" data-testid="navigation-logo">
-                <Avatar src={avatarSrc} alt="" data-testid="navigation-logo-image" />
-                <SiteName data-testid="navigation-logo-title">Federico Knüssel</SiteName>
-            </LogoLink>
-            <List>
-                <ListItem>
-                    <ListItemInternalLink to="/" data-testid="navigation-link">
+    <header className={headerStyle}>
+        <nav className={navigationMenuStyle}>
+            <GatsbyLink className={logoLinkStyle} to="/" data-testid="navigation-logo">
+                <img className={avatarStyle} src={avatarSrc} alt="" data-testid="navigation-logo-image" />
+                <span className={siteNameStyle} data-testid="navigation-logo-title">
+                    Federico Knüssel
+                </span>
+            </GatsbyLink>
+            <ul className={listStyle}>
+                <li className={listItemStyle}>
+                    <GatsbyLink className={linkStyle} to="/" data-testid="navigation-link">
                         Archive
-                    </ListItemInternalLink>
-                </ListItem>
-                <ListItem>
-                    <ListItemInternalLink to="/about" data-testid="navigation-link">
+                    </GatsbyLink>
+                </li>
+                <li className={listItemStyle}>
+                    <GatsbyLink className={linkStyle} to="/about" data-testid="navigation-link">
                         About
-                    </ListItemInternalLink>
-                </ListItem>
-                <ListItem>
-                    <ListItemExternalLink
+                    </GatsbyLink>
+                </li>
+                <li className={listItemStyle}>
+                    <a
+                        className={linkStyle}
                         target="_blank"
                         rel="noopener noreferrer"
                         href="https://github.com/fed"
                         data-testid="navigation-link"
                     >
                         GitHub
-                    </ListItemExternalLink>
-                </ListItem>
-                <ListItem>
-                    <ListItemExternalLink
+                    </a>
+                </li>
+                <li className={listItemStyle}>
+                    <a
+                        className={linkStyle}
                         target="_blank"
                         rel="noopener noreferrer"
                         href="https://twitter.com/fedknu"
                         data-testid="navigation-link"
                     >
                         Twitter
-                    </ListItemExternalLink>
-                </ListItem>
-                <ListItem>
-                    <ListItemExternalLink
+                    </a>
+                </li>
+                <li className={listItemStyle}>
+                    <a
+                        className={linkStyle}
                         target="_blank"
+                        // The `me` rel is required by Mastodon for verification purposes
+                        // eslint-disable-next-line react/no-invalid-html-attribute
                         rel="me noopener noreferrer"
                         href="https://mas.to/@fed"
                         data-testid="navigation-link"
                     >
                         Mastodon
-                    </ListItemExternalLink>
-                </ListItem>
-            </List>
-        </NavigationMenu>
-    </Header>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 );
