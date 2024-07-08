1a880e4d906049292a09ec477a3ca40412b73146
Federico Knüssel <fknussel@gmail.com>
2024-07-08 18:55:00 +1000

    Always render the same icon on main navigation menu

--- src/ui/layout.tsx
+++ src/ui/layout.tsx
@@ -14,105 +14,73 @@ import {
     baseNavigationLinkStyle,
     dimmedSiteNameStyle,
 } from './layout.css';
-import fishEmojiSrc from '../assets/1f420.svg';
-import chickEmojiSrc from '../assets/1f423.svg';
-import penguinEmojiSrc from '../assets/1f427.svg';
-import whaleEmojiSrc from '../assets/1f433.svg';
 import engineerEmojiSrc from '../assets/1f468-200d-1f4bb.svg';
-import dinosaurEmojiSrc from '../assets/1f995.svg';
-import kangarooEmojiSrc from '../assets/1f998.svg';
-import parrotEmojiSrc from '../assets/1f99c.svg';
-import otterEmojiSrc from '../assets/1f9a6.svg';
-import flamingoEmojiSrc from '../assets/1f9a9.svg';
-import dogEmojiSrc from '../assets/1f9ae.svg';
-
-const EMOJIS = [
-    otterEmojiSrc,
-    flamingoEmojiSrc,
-    dogEmojiSrc,
-    parrotEmojiSrc,
-    fishEmojiSrc,
-    chickEmojiSrc,
-    penguinEmojiSrc,
-    whaleEmojiSrc,
-    dinosaurEmojiSrc,
-    kangarooEmojiSrc,
-    engineerEmojiSrc,
-];
 
 interface Props {
     children: React.ReactNode;
 }
 
-export const Layout: React.FunctionComponent<Props> = ({ children }) => {
-    const [emojiSrc, setEmojiSrc] = React.useState(null);
+export const Layout: React.FunctionComponent<Props> = ({ children }) => (
+    <>
+        <a className={skipLinkStyle} href="#main" data-testid="layout-skip-link">
+            Skip to main content
+        </a>
 
-    React.useEffect(() => {
-        setEmojiSrc(EMOJIS[Math.floor(Math.random() * EMOJIS.length)]);
-    }, []);
+        <header className={headerStyle}>
+            <nav className={navigationMenuStyle}>
+                <Link className={logoLinkStyle} to="/" data-testid="navigation-logo">
+                    <img className={emojiStyle} src={engineerEmojiSrc} alt="" data-testid="navigation-logo-image" />
+                    <span className={siteNameStyle} data-testid="navigation-logo-title">
+                        fedknu<span className={dimmedSiteNameStyle}>.com</span>
+                    </span>
+                </Link>
 
-    return (
-        <>
-            <a className={skipLinkStyle} href="#main" data-testid="layout-skip-link">
-                Skip to main content
-            </a>
+                <ul className={listStyle}>
+                    <li className={listItemStyle}>
+                        <Link className={baseNavigationLinkStyle} to="/about" data-testid="layout-navigation-link">
+                            About
+                        </Link>
+                    </li>
+                    <li className={listItemStyle}>
+                        <Link className={baseNavigationLinkStyle} to="/" data-testid="layout-navigation-link">
+                            Blog
+                        </Link>
+                    </li>
+                    <li className={listItemStyle}>
+                        <a
+                            className={baseNavigationLinkStyle}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://github.com/fed"
+                            data-testid="layout-navigation-link"
+                        >
+                            GitHub
+                        </a>
+                    </li>
+                    <li className={listItemStyle}>
+                        <a
+                            className={baseNavigationLinkStyle}
+                            target="_blank"
+                            // The `me` rel is required by Mastodon for verification purposes
+                            // eslint-disable-next-line react/no-invalid-html-attribute
+                            rel="me noopener noreferrer"
+                            href="https://mas.to/@fed"
+                            data-testid="layout-navigation-link"
+                        >
+                            Mastodon
+                        </a>
+                    </li>
+                    <li className={listItemStyle}>
+                        <a className={baseNavigationLinkStyle} href="/rss.xml" data-testid="layout-navigation-link">
+                            RSS
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
 
-            <header className={headerStyle}>
-                <nav className={navigationMenuStyle}>
-                    <Link className={logoLinkStyle} to="/" data-testid="navigation-logo">
-                        {emojiSrc ? <img className={emojiStyle} src={emojiSrc} alt="" data-testid="navigation-logo-image" /> : null}
-                        <span className={siteNameStyle} data-testid="navigation-logo-title">
-                            fedknu<span className={dimmedSiteNameStyle}>.com</span>
-                        </span>
-                    </Link>
-
-                    <ul className={listStyle}>
-                        <li className={listItemStyle}>
-                            <Link className={baseNavigationLinkStyle} to="/about" data-testid="layout-navigation-link">
-                                About
-                            </Link>
-                        </li>
-                        <li className={listItemStyle}>
-                            <Link className={baseNavigationLinkStyle} to="/" data-testid="layout-navigation-link">
-                                Blog
-                            </Link>
-                        </li>
-                        <li className={listItemStyle}>
-                            <a
-                                className={baseNavigationLinkStyle}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                href="https://github.com/fed"
-                                data-testid="layout-navigation-link"
-                            >
-                                GitHub
-                            </a>
-                        </li>
-                        <li className={listItemStyle}>
-                            <a
-                                className={baseNavigationLinkStyle}
-                                target="_blank"
-                                // The `me` rel is required by Mastodon for verification purposes
-                                // eslint-disable-next-line react/no-invalid-html-attribute
-                                rel="me noopener noreferrer"
-                                href="https://mas.to/@fed"
-                                data-testid="layout-navigation-link"
-                            >
-                                Mastodon
-                            </a>
-                        </li>
-                        <li className={listItemStyle}>
-                            <a className={baseNavigationLinkStyle} href="/rss.xml" data-testid="layout-navigation-link">
-                                RSS
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-
-            <main className={contentStyle} id="main" data-testid="layout-content">
-                {children}
-            </main>
-        </>
-    );
-};
+        <main className={contentStyle} id="main" data-testid="layout-content">
+            {children}
+        </main>
+    </>
+);
