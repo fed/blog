f5358d9dc035a5b69446acfb8789aab1bc68979b
Federico Knüssel <fknussel@gmail.com>
2025-07-02 13:04:18 +1000

    Upgrade to Jest v30

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'; // eslint-disable-line import/no-extraneous-dependencies
-import { FlatCompat } from '@eslint/eslintrc'; // eslint-disable-line import/no-extraneous-dependencies
-import js from '@eslint/js'; // eslint-disable-line import/no-extraneous-dependencies
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 import typescriptEslint from '@typescript-eslint/eslint-plugin'; // eslint-disable-line import/no-unresolved
 import tsParser from '@typescript-eslint/parser'; // eslint-disable-line import/no-unresolved
 import { defineConfig, globalIgnores } from 'eslint/config'; // eslint-disable-line import/no-unresolved
 import _import from 'eslint-plugin-import';
 import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
-import globals from 'globals'; // eslint-disable-line import/no-extraneous-dependencies
+import globals from 'globals';
 
 const __filename = fileURLToPath(import.meta.url); // eslint-disable-line no-underscore-dangle
 const __dirname = path.dirname(__filename); // eslint-disable-line no-underscore-dangle
